f67afb651159383e70a9c261b430ad7224b1f838
Kickbykick <kiibatitoo@gmail.com>
2023-02-28 15:20:02 -0600

    fist commit

--- src/web/App.tsx
+++ src/web/App.tsx
@@ -1,13 +1,37 @@
 import { useState } from 'react';
 import './App.css';
+// import "./output.css";
 
 export const App = () => {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="container">
+    <div className="container bg-white">
+      <h1 className="underline">
+        Hello world!
+      </h1>
+      <p className="text-6xl mb-5">
+        Welcome ---- New Application!
+      </p>
+      <div className="flex flex-col items-center mt-16">
+       <h1 className="text-4xl text-center">
+         Tailwind CSS makes styling React components easier!
+       </h1>
+       <button className="bg-black text-white p-2.5 w-fit mt-9 ">
+         Get Started
+       </button>
+     </div>
+
+      <div className="flex flex-col items-center mt-16">
+       <h1 className="text-4xl text-center">
+        Tailwind CSS makes styling React components easier!
+       </h1>
+       <button className="bg-black text-white italic text-amber-900	 text-lg	p-2.5 w-fit mt-9 ">
+         Get Started
+       </button>
+     </div>
       <h1>{count}</h1>
-      <button onClick={() => setCount((count) => count + 1)}>Count</button>
+      <button onClick={() => setCount((count) => count + 1)}>Countaf asf</button>
     </div>
   );
 };
--- webpack.config.ts
+++ webpack.config.ts
@@ -23,7 +23,7 @@ const common: Configuration = {
       },
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        use: [MiniCssExtractPlugin.loader, 'css-loader', "postcss-loader"],
       },
       {
         test: /\.(ico|png|svg|eot|woff?2?)$/,
